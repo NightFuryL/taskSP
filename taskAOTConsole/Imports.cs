ce920f7d83bfc20eac8b27cf995ddaa0ceccb35e
NightFuryL <levodesskiy@gmail.com>
2026-03-22 11:47:18 +0200

    practice AOT 22.03.2026 with publish

---
+++ taskAOTConsole/Imports.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+namespace taskAOTConsole
+{
+    public static class Imports
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+    }
+}
--- taskAOTConsole/Program.cs
+++ taskAOTConsole/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace taskAOTConsole
+﻿namespace taskAOTConsole;
+class Program
 {
-    internal class Program
+
+
+    static void Main()
     {
-        static void Main(string[] args)
+        bool playAgain = true;
+
+        while (playAgain)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Загадайте число від 0 до 100");
+            int low = 0;
+            int high = 100;
+            string response;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                Imports.MessageBox(IntPtr.Zero,
+                    $"Your number {mid}?\nYes - yes\nNo - more\nCancel - less",
+                    "Guess the number",
+                    3);
+
+                Console.Write("Enter your response (yes/more/less): ");
+                response = Console.ReadLine();
+
+                if (response == "yes")
+                {
+                    Imports.MessageBox(IntPtr.Zero, "I guessed it!", "Result", 0);
+                    break;
+                }
+                else if (response == "more")
+                {
+                    low = mid + 1;
+                }
+                else if (response == "less")
+                {
+                    high = mid - 1;
+                }
+            }
+            Console.Write("Play again? (y/n): ");
+            playAgain = Console.ReadLine() == "y";
         }
     }
 }
